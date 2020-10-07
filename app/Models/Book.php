ed1ff644183bb799ecb3195a4748ee534c6414ef
Dea19 <dea.mulaj@gmail.com>
2020-10-07 17:39:46 +0200

    adds new path to book Resource

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -7,7 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Book extends Model
 {
-    use HasFactory;
 
     protected $guarded = [];
+
+    public function path()
+    {
+        return '/books/' . $this->id;
+
+    }
 }
